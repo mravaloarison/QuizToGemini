62e94f52dbbaec1ceea2fcdd8fe98a9a16374384
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-12-13 11:56:30 -0500

    UI half ready

---
+++ Quiz/CameraModel.swift
@@ -0,0 +1,8 @@
+//
+//  CameraModel.swift
+//  Quiz
+//
+//  Created by Mami Ravaloarison on 12/11/24.
+//
+
+import Foundation
---
+++ Quiz/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  Quiz
+//
+//  Created by Mami Ravaloarison on 12/11/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
