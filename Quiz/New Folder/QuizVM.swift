e5f31fbf859f322439b9d541d888833c9c0feaa7
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-01-08 14:20:29 -0500

    Prompt

--- Quiz/New Folder/QuizVM.swift
+++ Quiz/New Folder/QuizVM.swift
@@ -34,7 +34,7 @@ class QuizVM: ObservableObject {
     func fetchAIResponse(image: UIImage) async {
         do {
             let model = GenerativeModel(name: "gemini-1.5-flash", apiKey: APIKeyManager.default)
-            let prompt = "Describe the pic in one sentence"
+            let prompt = "Solve it if it's a quiz and give the response only, no explanation needed, if not just say it's not a quiz and that's all you need to do."
             let response = try await model.generateContent(image, prompt)
             if let text = response.text {
                 DispatchQueue.main.async {
